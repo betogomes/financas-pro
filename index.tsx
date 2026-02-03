3b0daa3a2b1e49b2ab7a2926fed9b5bb609af30b
betogomes <rgomes.silva@gmail.com>
2026-02-03 12:16:24 -0300

    fix: remove importmap e limpa configuracao de build

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Build Version: 1.2.4 - Clean Mount
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -14,3 +14,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+/*arquivo salvo as 12:00*/
+
